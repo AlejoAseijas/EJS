0437c18b862e2e450040548f5a7ffb781111b72f
Alejo Agustín Martinez Aseijas <54213917+AlejoAseijas@users.noreply.github.com>
2023-04-27 23:18:04 -0300

    initial

---
+++ desafios/main.c
@@ -0,0 +1,15 @@
+/*
+ Escribe un programa en C que simule el funcionamiento de un sistema de gestión de inventario de una tienda.
+ El programa debe permitir al usuario:
+
+ agregar, modificar y eliminar productos del inventario,
+ y también debe permitir al usuario ver una lista de todos los productos en el inventario.
+
+ Cada producto debe tener un nombre, una descripción, un precio y una cantidad en stock.
+
+ Además, el programa debe permitir al usuario realizar ventas de productos, disminuyendo la cantidad en stock del producto correspondiente y mostrando el total de la venta.
+ El programa también debe permitir al usuario generar un informe de ventas, mostrando la cantidad de cada producto vendido y el total de ventas para cada producto.
+ El inventario y las ventas deben almacenarse en archivos separados para poder recuperarlos después de que el programa se cierre y vuelva a abrir.
+ */
+
+#include <stdio.h>
---
+++ desafios/models/producto.c
@@ -0,0 +1,7 @@
+typedef struct
+{
+    char nombre[50];
+    char descripcion[200];
+    float precio;
+    int stock
+} Producto;
